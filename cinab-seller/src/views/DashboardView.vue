4e93e718d9e3f85a855e68721b858a7053f83030
Kelvin Kimutai <kiruikevinkim@gmail.com>
2023-12-13 17:07:01 +0300

    added sidebar

---
+++ cinab-seller/src/views/DashboardView.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div class="dash text-black dark:text-white">
+    <h1>This is an dashboard page</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ cinab-seller/src/views/InventoryView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="grid grid-cols-2 md:grid-cols-4">
+
+<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img class="pl-8 rounded-t-lg" src="../assets/cinab.jpg" alt="product image" />
+    </a>
+    <div class="px-5 pb-5">
+        <a href="#">
+            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+        </a>
+        <div class="flex items-center mt-2.5 mb-5">
+            <div class="flex items-center space-x-1 rtl:space-x-reverse text-gray-800">
+               Stock
+            </div>
+            <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">50</span>
+        </div>
+        <div class="flex items-center justify-between">
+            <span class="text-2xl font-bold text-gray-900 dark:text-white">$599</span>
+            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+        </div>
+    </div>
+</div>
+
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+    
+</style>
