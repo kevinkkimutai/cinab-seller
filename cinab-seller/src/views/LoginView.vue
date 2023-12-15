c90c54f3e152c0bdada3fa3e5b4273351a0399b2
Kelvin Kimutai <kiruikevinkim@gmail.com>
2023-12-15 14:43:20 +0300

    :lipstick: styles the forms

--- cinab-seller/src/views/LoginView.vue
+++ cinab-seller/src/views/LoginView.vue
@@ -1,12 +1,12 @@
 <template>
-  <section class="bg-gray-50 dark:bg-gray-900">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+  <section class="bg-gray- dark:bg-gray-900">
+    <div class="flex flex-col items-center  px-6 py-8 mx-auto md:h-screen lg:py-0">
       <div
-        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+        class="w-full bg-white border rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
       >
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1
-            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+            class="text-xl font-bold text-center leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
           >
             Sign in to your account
           </h1>
@@ -70,7 +70,7 @@
             </button>
             <p class="text-sm font-light text-gray-500 dark:text-gray-400">
               Don’t have an account yet?
-              <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+              <a href="/vendor" class="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >Sign up</a
               >
             </p>
