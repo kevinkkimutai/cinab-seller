987ecae85685b54fa366e5566a65683e6b90e7c5
Kelvin Kimutai <kiruikevinkim@gmail.com>
2023-12-19 13:46:53 +0300

    added components and pages

---
+++ frontend/src/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as DashboardPanels} from "../components/DashboardPanels";
+export { default as NavBar} from "../components/NavBar";
+export { default as TableProducts} from "../components/TableProducts";
+export { default as TableResents} from "../components/TableResents";
