245decf6157c52a24815d645840707a2d15e03a5
Kelvin Kimutai <kiruikevinkim@gmail.com>
2023-12-19 14:19:00 +0300

    updated styles

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavBar() {
   return (
 <>
-<nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+   <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
   <div class="px-3 py-3 lg:px-5 lg:pl-3">
     <div class="flex items-center justify-between">
       <div class="flex items-center justify-start rtl:justify-end">
