8321e440afa04f8687ab15407b46431e28ae855f
Kelvin Kimutai <kiruikevinkim@gmail.com>
2023-12-19 16:25:36 +0300

    added product get and post

--- server/migrations/20231219125109-create-product.js
+++ server/migrations/20231219125109-create-product.js
@@ -21,6 +21,9 @@ module.exports = {
       image: {
         type: Sequelize.STRING
       },
+      userId: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
