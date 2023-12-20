8464d0068c72ce9741080111215046f360a1ba12
Kelvin Kimutai <117738367+kevinkkimutai@users.noreply.github.com>
2023-12-20 12:05:31 +0300

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -55,7 +55,7 @@ export default function NavBar() {
   </div>
 </nav>
 
-<!-- sidebar -->
+
 <aside id="logo-sidebar" class="fixed top-14 left-0 z-50 w-56 h-screen pt-4 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-green-800 dark:border-gray-700" aria-label="Sidebar">
    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
