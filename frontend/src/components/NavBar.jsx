95354c79c2996546c2d96a58afec8a991c7ba22d
Kelvin Kimutai <117738367+kevinkkimutai@users.noreply.github.com>
2023-12-20 12:30:54 +0300

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-
+import Image from "../assets/cinablogo.png";
+import { BiSun, BiMoon } from "react-icons/bi";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function NavBar() {
 
   return (
     <>
+  
 <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
   <div class="px-3 py-3 lg:px-5 lg:pl-3">
     <div class="flex items-center justify-between">
@@ -20,12 +23,12 @@ export default function NavBar() {
           <span class="self-center text-xl font-semibold sm:text-2xl text-black whitespace-nowrap dark:text-white">Cinab-Seller</span>
         </a>
       </div>
-      <div class="flex items-center">
+      <div class="flex items-center">G
           <div class="flex items-center ms-3">
             <div>
               <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                 <span class="sr-only">Open user menu</span>
-                <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo" />
+                <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo"/>
               </button>
             </div>
             <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
@@ -51,6 +54,7 @@ export default function NavBar() {
     </div>
   </div>
 </nav>
+
 
 <aside id="logo-sidebar" class="fixed top-14 left-0 z-50 w-56 h-screen pt-4 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-green-800 dark:border-gray-700" aria-label="Sidebar">
    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
@@ -109,6 +113,8 @@ export default function NavBar() {
       </ul>
    </div>
 </aside>
+
+
     </>
   );
 }
