27b922d944be6e3a3021e00608fce15e5ada5679
Kelvin Kimutai <117738367+kevinkkimutai@users.noreply.github.com>
2023-12-20 12:56:19 +0300

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Image from "../assets/cinablogo.png";
-import { BiSun, BiMoon } from "react-icons/bi";
-import { useStateContext } from "../contexts/ContextProvider";
 
 export default function NavBar() {
 
@@ -55,7 +52,7 @@ export default function NavBar() {
 </nav>
 
 
-<aside id="logo-sidebar" class="fixed top-14 left-0 z-50 w-56 h-screen pt-4 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-green-800 dark:border-gray-700" aria-label="Sidebar">
+<aside id="logo-sidebar" class="fixed top-14 left-0 z-40 w-56 h-screen pt-4 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-green-800 dark:border-gray-700" aria-label="Sidebar">
    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
          <li>
