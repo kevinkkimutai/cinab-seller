0973bcd724af04c7d5b2390564fe690246ad7003
Kelvin Kimutai <117738367+kevinkkimutai@users.noreply.github.com>
2023-12-20 13:57:32 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,11 @@ import { Routes, Route,  } from "react-router-dom";
 import {
   Dashboard,
   Inventory,
-  Login,
   Offers,
   Orders,
   Products,
   UploadForm,
   VendorDetails,
-  Sales,
 } from "./pages";
 import { NavBar } from "./components";
 import { useStateContext } from "./contexts/ContextProvider";
@@ -27,26 +25,20 @@ useEffect(() => {
   }
 }, [setCurrentColor, setCurrentMode]);
   return (
-<>
-    <div className={currentMode === "Light" ? "dark" : ""}>
-      <NavBar />
-      <div className="md:w-10/12 md:ml-56 p-2 pt-20 dark:bg-gray-900  max-h-screen h-screen">
-      <Routes >
-          <Route index element={<Dashboard />} />
-          <Route path="/inventory" element={<Inventory />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/offers" element={<Offers />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/productform" element={<UploadForm />} />
-          <Route path="/vendor" element={<VendorDetails />} />
-          <Route path="/sales" element={<Sales />} />
-          <Route path="/login" element={<Login />} />
-
-        </Routes>
-      </div>
-
+    <div className={`min-h-screen ${currentMode === "Light" ? "dark h-screen" : "h-screen"}`}>
+    <NavBar />
+    <div className={`md:w-10/12 md:ml-56 p-2 pt-20 dark:bg-gray-900 h-full`}>
+      <Routes>
+        <Route index element={<Dashboard />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/productform" element={<UploadForm />} />
+        <Route path="/vendor" element={<VendorDetails />} />
+      </Routes>
     </div>
-  </>
+  </div>
   
    
   );
