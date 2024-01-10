f247651b4978720ab0dcc5de94998264ef1734a1
Kenyansa <kenyansajustine@gmail.com>
2024-01-10 15:04:06 +0300

    add payment details form

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -17,8 +17,8 @@ export default function Settings () {
                     {/* Center column */}
                     <div class="md:w-1/3 mx-2 my-4">
                     <div class="bg-gray-100 p-4 rounded-lg mb-4">
-                        <p class="text-sm font-medium text-gray-700">Username:</p>
-                        <p class="text-lg font-semibold text-gray-900 dark:text-white">[Username Prop]</p>
+                        <p class="text-sm font-medium text-gray-700">Company name:</p>
+                        <p class="text-lg font-semibold text-gray-900 dark:text-white">[Company name Prop]</p>
                     </div>
                     <div class="bg-gray-100 p-4 rounded-lg">
                         <p class="text-sm font-medium text-gray-700">Email:</p>
@@ -64,6 +64,7 @@ export default function Settings () {
                     <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">User Profile tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
                 </div>
                 <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="business-info" role="tabpanel" aria-labelledby="business-info-tab">
+                    {/* Business iNFO div */}
                     <form class="p-2">
                         <div class="py-5">
                             <h1 class="text-semibold text-gray-700 dark:text-gray-50 text-xl">Business Information</h1>
@@ -109,9 +110,117 @@ export default function Settings () {
                         </div>
                     </form>
                 </div>            
-                <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="payment-info" role="tabpanel" aria-labelledby="payment-info-tab">
+                {/* <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="payment-info" role="tabpanel" aria-labelledby="payment-info-tab">
                     <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Payment-Info tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
+                </div> */}
+
+                <div className="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="payment-info" role="tabpanel" aria-labelledby="payment-info-tab">
+                    <div className="py-5">
+                        <h1 className="text-semibold text-gray-700 dark:text-gray-50 text-xl">
+                            Preferred payment option:
+                        </h1>
+                        <p
+                        className="mt-1 text-sm text-gray-800 dark:text-gray-50"
+                        id="user_avatar_help"
+                        >
+                            Select the payment method, if applicable, of your choice, and ensure to provide all required details. We'll review the validity of your documents upon submission.
+                        </p>
+                    </div>
+                    <div className="mt-4">
+                        <div className="border-b border-gray-300 mb-4">
+                            <h2 className="text-gray-500 font-medium dark:text-gray-50">
+                                Bank Account Details
+                            </h2>
+                        </div>
+                    <div className="grid grid-cols-2 sm:grid-cols-2  gap-4 gap-y-7 ">
+                        <div>
+                            <label
+                            htmlFor="AccountNumber"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Bank Account Number{" "}
+                            </label>
+                            <input
+                            type="text"
+                            id="AccountNumber"
+                            min="0"
+                            name="AccountNumber"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            laceholder="02275656597565"
+                            required 
+                            />
+                        </div>
+                        <div>
+                            <label
+                            htmlFor="BankName"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Bank Name{" "}
+                            </label>
+                            <input
+                            type="text"
+                            id="BankName"
+                            name="BankName"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Equity"
+                            required
+                            />
+                        </div>
+                    </div>
+                    <div className="border-b border-gray-300 mb-4 mt-4 md:mt-11">
+                        <h2 className="text-gray-500 font-medium dark:text-gray-50">
+                            Mpesa Details
+                        </h2>
+                    </div>
+                    <div className="grid grid-cols-2 sm:grid-cols-2  gap-4 gap-y-7 ">
+                        <div>
+                            <label
+                            htmlFor="MpesaNumber"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                            MPesa Number{" "}
+                            </label>
+                            <input
+                            type="number"
+                            min="0"
+                            max="9999999999999"
+                            id="MpesaNumber"
+                            name="MpesaNumber"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="+254782588855"
+                            required
+                            />
+                        </div>
+                        <div>
+                            <label
+                            htmlFor="MpesaName"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                            Full Mpesa Name{" "}
+                            </label>
+                            <input
+                            type="text"
+                            id="MpesaName"
+                            name="MpesaName"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="John Doe"
+                            required
+                            />
+                        </div>
+                    </div>
                 </div>
+
+                <div className="flex justify-between py-6">
+                    <button
+                    type="submit"
+                    className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md"
+                    >
+                    Submit{" "}
+                    </button>
+                </div>
+                </div>
+
+
                 <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="shipping-info" role="tabpanel" aria-labelledby="shipping-info-tab">
                     <p class="text-sm text-gray-500 dark:text-gray-400">This is some placeholder content the <strong class="font-medium text-gray-800 dark:text-white">Shipping-Info tab's associated content</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classes to control the content visibility and styling.</p>
                 </div>
