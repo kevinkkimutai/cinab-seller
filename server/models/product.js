43fcbbb8a5be442b3ebebde8f35a368b2e4f1e23
MURIITHIABEL <abelmuriithi04@gmail.com>
2024-01-12 11:27:31 +0300

    product table

--- server/models/product.js
+++ server/models/product.js
@@ -21,50 +21,48 @@ module.exports = (sequelize) => {
 
   Product.init(
     {
-      pname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      brand: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      category: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      vendorId: {
+      id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
-        onUpdate: "CASCADE",
-        onDelete: "CASCADE",
-        references: { model: "vendors", key: "id" },
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: true
       },
-      image: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      price: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-      },
-      Rprice: {
-        type: DataTypes.FLOAT,
-        allowNull: true,
-      },
-      approval: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: false,
-      },
-      stock: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+      category_id: { type: DataTypes.INTEGER, allowNull: true },
+      subcategory_id: { type: DataTypes.INTEGER, allowNull: true },
+      childcategory_id: { type: DataTypes.INTEGER, allowNull: true },
+      tax_id: { type: DataTypes.INTEGER, allowNull: true },
+      brand_id: { type: DataTypes.INTEGER, allowNull: true },
+      name: { type: DataTypes.TEXT, allowNull: true },
+      slug: { type: DataTypes.TEXT, allowNull: true },
+      sku: { type: DataTypes.STRING, allowNull: true },
+      tags: { type: DataTypes.TEXT, allowNull: true },
+      video: { type: DataTypes.TEXT, allowNull: true },
+      sort_details: { type: DataTypes.TEXT, allowNull: true },
+      specification_name: { type: DataTypes.TEXT, allowNull: true },
+      specification_description: { type: DataTypes.TEXT, allowNull: true },
+      is_specification: { type: DataTypes.INTEGER, allowNull: true },
+      details: { type: DataTypes.TEXT, allowNull: true },
+      photo: { type: DataTypes.STRING, allowNull: true },
+      discount_price: { type: DataTypes.DOUBLE, allowNull: true },
+      previous_price: { type: DataTypes.DOUBLE, allowNull: true },
+      stock: { type: DataTypes.INTEGER, allowNull: true },
+      meta_keywords: { type: DataTypes.TEXT, allowNull: true },
+      meta_description: { type: DataTypes.TEXT, allowNull: true },
+      status: { type: DataTypes.INTEGER, allowNull: true },
+      is_type: { type: DataTypes.STRING, allowNull: true },
+      date: { type: DataTypes.STRING, allowNull: true },
+      file: { type: DataTypes.STRING, allowNull: true },
+      link: { type: DataTypes.TEXT, allowNull: true },
+      file_type: { type: DataTypes.STRING, allowNull: true },
+      created_at: { type: DataTypes.DATE, allowNull: true },
+      updated_at: { type: DataTypes.DATE, allowNull: true },
+      license_name: { type: DataTypes.TEXT, allowNull: true },
+      license_key: { type: DataTypes.TEXT, allowNull: true },
+      item_type: { type: DataTypes.STRING, allowNull: true },
+      thumbnail: { type: DataTypes.STRING, allowNull: true },
+      affiliate_link: { type: DataTypes.TEXT, allowNull: true },
+      createdAt: { type: DataTypes.DATE, allowNull: true },
+      updatedAt: { type: DataTypes.DATE, allowNull: true }
     },
     {
       sequelize,
