2e7ac8d8bea1b75531e80fadc33af7b967a6d8df
Mosesochacha <ochachamoses443@gmail.com>
2024-01-15 11:30:12 +0300

    Done fetching data

--- server/models/subcategory.js
+++ server/models/subcategory.js
@@ -1,28 +1,31 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class subcategory extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      subcategory.belongsTo(models.category, {
+        as: "category",
+        foreignKey: "category_id",
+      });
+      subcategory.hasMany(models.chield_category, {
+        as: "childcategories",
+        foreignKey: "subcategory_id",
+      });
     }
   }
-  subcategory.init({
-    name: DataTypes.STRING,
-    slug: DataTypes.STRING,
-    category_id: DataTypes.INTEGER,
-    status: DataTypes.INTEGER,
-    created_at: DataTypes.DATE,
-    updated_at: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'subcategory',
-  });
+  subcategory.init(
+    {
+      name: DataTypes.STRING,
+      slug: DataTypes.STRING,
+      category_id: DataTypes.INTEGER,
+      status: DataTypes.INTEGER,
+      created_at: DataTypes.DATE,
+      updated_at: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "subcategory",
+    }
+  );
   return subcategory;
 };
--- server/routes/itemsRoutes.js
+++ server/routes/itemsRoutes.js
@@ -3,15 +3,16 @@ const router = express.Router();
 const itemsController = require("../controllers/ItemsController");
 const uploadMiddleware = require("../middlewares/UploadingFiles");
 
-
 /**
  * Get all orders.
  */
 router.get("/items", itemsController.getAllitems);
+router.get("/category", itemsController.getData);
+router.post("/items", itemsController.createitems);
+router.delete("/items/:itemsId", itemsController.deleteitems);
 
 /**
  * Get a order by ID.
  */
-
 
 module.exports = router;
